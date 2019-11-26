1af44fd24e10ba28132db8da340f1190aa31824c
SamBritten <sbritten315@gmail.com>
2019-11-26 14:14:23 +0000

    configured remote state

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "azurerm" {
+    storage_account_name  = "${var.storage_account_name}"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+	version = "=1.30.1"
+}
+module "nginx" {
+  source = "./modules/nginx"
+}
