0d360a5c04097ed661728770c074baa284b92cdc
SamBritten <sbritten315@gmail.com>
2019-11-26 12:20:19 +0000

    working

--- network-security-group.tf
+++ network-security-group.tf
@@ -109,29 +109,7 @@ resource "azurerm_network_security_group" "manage" {
         protocol                   = "Tcp"
         source_port_range          = "*"
         destination_port_range     = "22"
-        source_address_prefix      = "10.10.1.0/24"
-        destination_address_prefix = "*"
-    }
-    security_rule {
-        name                       = "SSH2"
-        priority                   = 1002
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "22"
-        source_address_prefix      = "10.10.2.0/24"
-        destination_address_prefix = "*"
-    }
-    security_rule {
-        name                       = "SSH3"
-        priority                   = 1003
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "22"
-        source_address_prefix      = "10.10.4.0/24"
+        source_address_prefix      = "*"
         destination_address_prefix = "*"
     }
     security_rule {
